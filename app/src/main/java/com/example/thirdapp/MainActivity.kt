c23e743b706cbbf501bf2cfce92c347e1db37517
ji-circle <sp202401jw@gmail.com>
2024-10-15 12:47:52 +0900

    finish: 주석 정리

--- app/src/main/java/com/example/thirdapp/MainActivity.kt
+++ app/src/main/java/com/example/thirdapp/MainActivity.kt
@@ -46,8 +46,6 @@ import androidx.navigation.compose.rememberNavController
 import com.example.thirdapp.ui.theme.ThirdAppTheme
 import kotlin.math.pow
 
-//TODO navigate 값 전달하면서 and 전달하지 않으면서 ==> 코드 확인하기
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,13 +61,9 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "main") {
                     composable(route = "main") {
-                        //뷰모델 사용할거면 아래 코드 수정한다.
-//                        MainScreen(navController = navController)
-                        MainScreen{height, weight, selectedOption, checked -> //매개변수가 1개라서, ( ) 로 넣지 않아도 됨. 이렇게 중괄호에 바로 써도 됨
+                        MainScreen{height, weight, selectedOption, checked ->
                             viewModel.bmiCalculate(height, weight, selectedOption, checked)
                             // 위 코드로, viewmodel 안의 bmi 값이 업데이트된다.
-                            //아래 코드에서 넘겨준다... 근데 이럴 필요가 없음! 뷰모델이 이런 스크린들 안에 있는 게 아니라, 위에 있음.
-//                            navController.navigate(route = "analysis/$bmi")
                             navController.navigate(route = "analysis")
                         }
                     }
@@ -78,7 +72,6 @@ class MainActivity : ComponentActivity() {
                     composable(route = "analysis") {
                         AnalysisScreen(
                             navController = navController,
-//                            result = it.arguments?.getString("value") ?: ""
                             result = bmi
                         )
                     }
@@ -92,14 +85,11 @@ class MainActivity : ComponentActivity() {
 class BmiViewModel : ViewModel() {
     //viewModel에서는 remembersaveable을 사용할 필요가 없음...
     //  어차피 composable function 안에 있는 게 아니기때문에, composable function이 다시 호출되더라도 다시 기억할 필요가 없음...
-//    var result by rememberSaveable { mutableStateOf("Default") }
     private val _result = mutableStateOf("Default")
     val result: State<String> = _result
-    //var이 아니라 val, by가 아니라 = 으로 관리...
     // result 안에서, 따로 로직을 통해서만 값을 바꿀 수 있게 하고 싶을 때.
     //  수정은 안되고 읽기만 되게 하게 하고 싶으면
     //    private 로 _ 포함해 만들고, 밑에 val으로 추가.
-    //mutablestateof는 읽기, 쓰기 다 가능하긴 함...
     // State는 읽기만 가능
 
     fun bmiCalculate(
@@ -142,20 +132,12 @@ class BmiViewModel : ViewModel() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
-    //뷰모델 사용하게 되면, 여기서 네비게이션 할 게 아니기 때문에 아래 코드 주석처리한다
-//    navController: NavController,
-    //추가!
     onResultClicked: (Double, Double, String, Boolean) -> Unit
 ) {
     var height by rememberSaveable { mutableStateOf("") }
     var weight by rememberSaveable { mutableStateOf("") }
     var selectedOption by rememberSaveable { mutableStateOf("Normal") }
     var checked by rememberSaveable { mutableStateOf(false) }
-    //mainscreen이 없어지더라도, 유의미한 결과인 result는 생명주기를 app이랑 같이 하면 좋음...
-    //  그리고 result 값을 더 체계적으로 관리하기 위해서 ... viewModel을 사용함
-    //  아래 코드는 뷰모델 사용하기 전까지임... 뷰모델 사용할 땐 아래 코드 지운다!
-//    var result by rememberSaveable { mutableStateOf("Default") }
-
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -196,7 +178,6 @@ fun MainScreen(
             ElevatedButton(
                 modifier = Modifier.fillMaxWidth(),
                 onClick = {
-                    //추가!
                     if (height.isNotEmpty() && weight.isNotEmpty()) {
                         onResultClicked(
                             height.toDouble(),
@@ -205,11 +186,8 @@ fun MainScreen(
                             checked
                         )
                     }
-                    //아래 내용을 onResultClicked에서 하는거로 변경...
-//                    navController.navigate(route = "analysis/$result")
                 }
             ) {
-                //
                 Text("Enter")
             }
         }
@@ -283,209 +261,3 @@ fun CheckBoxSet(checked: Boolean, onChange: (Boolean) -> Unit) {
         )
     }
 }
-//
-//
-//
-//class MainActivity : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContent {
-//            ThirdAppTheme {
-//                val viewModel = viewModel<BmiViewModel>()
-//
-//                val bmi = viewModel.result.value
-//
-//                val navController = rememberNavController()
-//                NavHost(navController = navController, startDestination = "main") {
-//                    composable(route = "main") {
-//                        MainScreen{ height, weight, selectedOption, checked ->
-//                            viewModel.bmiCalculate(height, weight, selectedOption, checked)
-//                        }
-//                    }
-//                    composable(route = "analysis") {
-//                        AnalysisScreen(
-//                            navController = navController,
-//                            result = bmi
-//                        )
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//class BmiViewModel : ViewModel() {
-//    private val _result = mutableStateOf("Default")
-//    val result: State<String> = _result
-//
-//    fun bmiCalculate(
-//        height: Double, weight: Double, selectedOption: String, checked: Boolean
-//    ) {
-//        val resultList = listOf(
-//            "Underweight",
-//            "Normal",
-//            "Overweight",
-//            "Obese",
-//            "Not Allowed"
-//        )
-//        val bmi = weight / (height / 100.0).pow(2.0)
-//        if (checked) {
-//            _result.value = resultList[4]
-//        }
-//        when (selectedOption) {
-//            "Simplified" -> {
-//                if (bmi >= 25.0) _result.value = resultList[2]
-//                else _result.value = resultList[1]
-//            }
-//
-//            "Normal" -> {
-//                if (bmi >= 25.0) _result.value = resultList[2]
-//                else if (bmi <= 18.5) _result.value = resultList[0]
-//                else _result.value = resultList[1]
-//            }
-//
-//            "Detailed" -> {
-//                if (bmi >= 30.0) _result.value = resultList[3]
-//                else if (bmi >= 25.0) _result.value = resultList[2]
-//                else if (bmi <= 18.5) _result.value = resultList[0]
-//                else _result.value = resultList[1]
-//            }
-//        }
-//    }
-//}
-//
-//
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun MainScreen(
-//    onResultClicked: (Double, Double, String, Boolean) -> Unit
-//) {
-//    var height by rememberSaveable { mutableStateOf("") }
-//    var weight by rememberSaveable { mutableStateOf("") }
-//    var selectedOption by rememberSaveable { mutableStateOf("Normal") }
-//    var checked by rememberSaveable { mutableStateOf(false) }
-//
-//
-//    Scaffold(
-//        modifier = Modifier.fillMaxSize(),
-//        topBar = {
-//            TopAppBar(title = { Text("Obesity Analysis") })
-//        }
-//    ) { innerPadding ->
-//        Column(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(innerPadding)
-//                .padding(16.dp)
-//        ) {
-//            OutlinedTextField(
-//                value = height,
-//                onValueChange = { height = it },
-//                label = { Text("Height") },
-//                modifier = Modifier.fillMaxWidth(),
-//                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-//            )
-//
-//            OutlinedTextField(
-//                value = weight,
-//                onValueChange = { weight = it },
-//                label = { Text("Weight") },
-//                modifier = Modifier.fillMaxWidth(),
-//                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-//            )
-//
-//            Spacer(modifier = Modifier.padding(vertical = 16.dp))
-//            Text(text = "Choose the analysis style.")
-//            RadioButtonSet(selectedOption = selectedOption, onChange = { selectedOption = it })
-//
-//            Spacer(modifier = Modifier.padding(vertical = 16.dp))
-//            CheckBoxSet(checked = checked, onChange = { checked = it })
-//
-//            Spacer(modifier = Modifier.padding(vertical = 16.dp))
-//            ElevatedButton(
-//                modifier = Modifier.fillMaxWidth(),
-//                onClick = {
-//                    if (height.isNotEmpty() && weight.isNotEmpty()) {
-//                        onResultClicked(
-//                            height.toDouble(),
-//                            weight.toDouble(),
-//                            selectedOption,
-//                            checked
-//                        )
-//                    }
-//                }
-//            ) {
-//                Text("Enter")
-//            }
-//        }
-//    }
-//}
-//
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun AnalysisScreen(navController: NavController, result: String) {
-//    Scaffold(
-//        modifier = Modifier.fillMaxSize(),
-//        topBar = {
-//            TopAppBar(
-//                title = { Text("Analysis Result") },
-//                navigationIcon = {
-//                    IconButton(onClick = { navController.navigateUp() }) {
-//                        Icon(
-//                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-//                            contentDescription = "back"
-//                        )
-//                    }
-//                }
-//            )
-//        }
-//    ) { innerPadding ->
-//        Column(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(innerPadding)
-//                .padding(16.dp),
-//            verticalArrangement = Arrangement.Center,
-//            horizontalAlignment = Alignment.CenterHorizontally
-//        ) {
-//            Text(text = "Your Obesity result is...", fontSize = 20.sp)
-//            Spacer(modifier = Modifier.padding(vertical = 16.dp))
-//            Text(text = result, fontSize = 30.sp)
-//        }
-//    }
-//}
-//
-//@Composable
-//fun RadioButtonSet(selectedOption: String, onChange: (String) -> Unit) {
-//    val radioOptions = listOf("Simplified", "Normal", "Detailed")
-//
-//    Column {
-//        radioOptions.forEach { i ->
-//            Row(
-//                verticalAlignment = Alignment.CenterVertically
-//            ) {
-//                RadioButton(
-//                    selected = (i == selectedOption),
-//                    onClick = { onChange(i) }
-//                )
-//                Text(text = i)
-//            }
-//        }
-//    }
-//}
-//
-//@Composable
-//fun CheckBoxSet(checked: Boolean, onChange: (Boolean) -> Unit) {
-//    Row(
-//        modifier = Modifier.padding(horizontal = 32.dp),
-//        horizontalArrangement = Arrangement.SpaceEvenly
-//    ) {
-//        Text("Do you agree to the analysis of your height and weight?")
-//        Checkbox(
-//            checked = checked,
-//            onCheckedChange = { onChange(it) },
-//            modifier = Modifier.padding(horizontal = 8.dp)
-//        )
-//    }
-//}
